a51b6a52a7b19a17b80ba0a498043b167e675d79
ethical.emmy@gmail.com <ethical.emmy@gmail.com>
2024-01-23 03:29:51 +0100

    updated the code

---
+++ main.js
@@ -0,0 +1,9 @@
+//--~~~~~~ REVIEW SWIPER~~~~~~//
+var swiper = new Swiper(".testimonial-wrapper", {
+  spaceBetween: 30,
+  loop: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
